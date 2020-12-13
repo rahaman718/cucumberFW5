e6c9e290bd7c7284653843b714afe7f364ba0ba3
rahaman718 <69428206+rahaman718@users.noreply.github.com>
2020-12-13 11:41:07 -0500

    new java class

---
+++ src/test/java/org/example/Gitclass.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Gitclass {
+    //This is a test java class for git practice
+    //Hopefully this will help me to be pro in git command
+    //this is my third practice run
+
+}
